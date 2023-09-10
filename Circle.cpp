f0f0e473bce32c45682949190992f38f5a6788dc
Slava_desktop <rsramzesslava@gmail.com>
2023-09-10 22:40:26 +0300

    Completed class

--- Circle.cpp
+++ Circle.cpp
@@ -1,1 +1,42 @@
 #include "Circle.h"
+
+const long double Circle::CircleMyPi = 3.14159265358979323851;
+
+Circle::Circle()
+{
+	coord_x = 0;
+	coord_y = 0;
+	radius = 0;
+}
+
+Circle::Circle(double x, double y, double r)
+{
+	coord_x = x;
+	coord_y = y;
+	radius = r;
+}
+
+double Circle::CircleLen()
+{
+	return (2 * CircleMyPi * radius); //Check brackets
+}
+
+double Circle::CircleArea()
+{
+	return (CircleMyPi * (radius * radius));
+}
+
+double Circle::CircleDistance(Circle obj2)
+{
+	return sqrt(pow(abs(coord_x - obj2.coord_x), 2) + pow(abs(coord_y - obj2.coord_y), 2));
+}
+
+bool Circle::CircleIntersect(const Circle& obj2)
+{
+	return (abs(radius - obj2.radius) < CircleDistance(obj2) < (radius + obj2.radius));
+}
+
+bool Circle::InCircle(double x, double y)
+{
+	return ((coord_x - x) * (coord_x - x) + (coord_y - y) * (coord_y - y) <= (radius * radius));
+}
--- Circle.h
+++ Circle.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <math.h>
+
 class Circle
 {
+	double coord_x, coord_y;
+	double radius;
+
+	static const long double CircleMyPi;
+
+	Circle();
+	Circle(double x, double y, double r);
+
+public:
+
+	double CircleDistance(Circle obj2);
+	double CircleLen();
+	double CircleArea();
+
+	bool CircleIntersect(const Circle& obj2);
+	bool InCircle(double x, double y);  //Including borders
 };
 
